c15352e90244f2604790ce4d9f8d7f7d4dc82c1e
suryapalla <suryapalla449@gmail.com>
2023-09-06 13:51:31 +0530

    debugged favicon error

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", { userName: "Surya" });
+  res.render("index");
+});
+
+app.get("/favicon.ico", (req, res) => {
+  res.sendStatus(200);
 });
 
 app.listen(3000);
